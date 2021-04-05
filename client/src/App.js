d1984d332a62ddfa44afe6f30401eb2d2a8677ca
mharikmert <mhilmiarikmert@gmail.com>
2021-04-05 22:32:23 +0300

    fetch coin price from api

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,23 @@
-import './App.css';
-
-function App() {
-  return (
-      <>
-      </>
-  );
+import Coin from './Coin'
+import './index.css'
+const App = () => {
+    const getData = (url, coin) => {
+        url += coin + 'BTC';
+        const xhr = new XMLHttpRequest();
+        xhr.open('GET',url, false);
+        xhr.send();
+        if(xhr.status === 200)
+                return JSON.parse(xhr.responseText)['price'];
+    }
+    const url = 'https://api.binance.com/api/v3/ticker/price?symbol=';
+    return (
+        <div className = 'container'>
+            <Coin name = 'BNB' price = {getData(url,'BNB')}> </Coin>
+            <Coin name = 'ETH' price = {getData(url, 'ETH')}> </Coin>
+            <Coin name = 'XRP' price = {getData(url, 'XRP')}> </Coin>
+            <Coin name = 'BCH' price = {getData(url, 'BCH')}> </Coin>
+            <Coin name = 'LTC' price = {getData(url, 'LTC')}> </Coin>
+        </div>)
 }
 
 export default App;
