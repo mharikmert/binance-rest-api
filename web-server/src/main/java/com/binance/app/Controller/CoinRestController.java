c14c4e254c72f6822124cb53eae3a19daa261a9c
mharikmert <mhilmiarikmert@gmail.com>
2021-04-06 21:51:12 +0300

    new endpoint to fetch and update all the records

--- web-server/src/main/java/com/binance/app/Controller/CoinRestController.java
+++ web-server/src/main/java/com/binance/app/Controller/CoinRestController.java
@@ -4,11 +4,16 @@ import com.binance.app.Repository.CoinRepository;
 import com.binance.app.Model.Coin;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.configurationprocessor.json.JSONArray;
+import org.springframework.boot.configurationprocessor.json.JSONException;
+import org.springframework.boot.configurationprocessor.json.JSONObject;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class CoinRestController {
@@ -18,6 +23,18 @@ public class CoinRestController {
 
     @Value("${secretKey}")
     private String secretKey;
+
+    @Bean
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
+
+    //injects restTemplate bean to fetch data
+    private RestTemplate restTemplate;
+    @Autowired
+    public void setRestTemplate (RestTemplate restTemplate){
+        this.restTemplate = restTemplate;
+    }
 
     //inject the coin repository to use CRUD operations
     private CoinRepository coinRepository;
@@ -30,11 +47,32 @@ public class CoinRestController {
     @RequestMapping(value = "/api/v1/coins", method = RequestMethod.POST)
     public ResponseEntity<?> saveCoins(@RequestBody Coin coin) {
         coinRepository.save(coin);
-        System.out.println(coin);
         return ResponseEntity.ok().build();
     }
+
     @RequestMapping(value = "api/v1/coins", method = RequestMethod.GET)
     public ResponseEntity<?> coins(){
         return ResponseEntity.ok(coinRepository.findAll());
+    }
+
+    //fetches all bitcoin current price and saves to postgre
+    @RequestMapping(value = "/api/v1/fetchAllBtc", method = RequestMethod.GET)
+    public ResponseEntity<?> fetchAllBTC() throws JSONException {
+        String url = "https://api.binance.com/api/v3/ticker/price";
+        //returns List of JSON objects -> {"symbol": "SOMEBTC", "price": "price"}
+        Object[] data = restTemplate.getForObject(url, Object[].class); // json directly?
+        assert data != null;
+        //converts obj to json array
+        JSONArray obj = new JSONArray(data);
+        //json parse
+        for(int i = 0; i < obj.length(); i++) {
+            JSONObject jsonObject = (JSONObject) obj.get(i); // have price and symbol
+            //type conversion
+            String symbol = (String) jsonObject.get("symbol");
+            double price = Double.parseDouble((String) jsonObject.get("price"));
+            //save data as coin
+            coinRepository.save(new Coin(symbol, price));
+        }
+        return ResponseEntity.ok(data);
     }
 }
