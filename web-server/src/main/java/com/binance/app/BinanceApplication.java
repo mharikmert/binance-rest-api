57682335b5ab6d1fe147b7f8e2eab28c00b15525
mharikmert <mhilmiarikmert@gmail.com>
2021-04-06 20:22:03 +0300

    name change

--- web-server/src/main/java/com/binance/app/BinanceApplication.java
+++ web-server/src/main/java/com/binance/app/BinanceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WebServerApplication {
+public class BinanceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(WebServerApplication.class, args);
+        SpringApplication.run(BinanceApplication.class, args);
     }
 
 }
