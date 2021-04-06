dcb1a9bfe03499b7e9f7c4af5b378fddfa7d536d
mharikmert <mhilmiarikmert@gmail.com>
2021-04-06 20:22:45 +0300

    data field edition for model class

--- web-server/src/main/java/com/binance/app/Model/Coin.java
+++ web-server/src/main/java/com/binance/app/Model/Coin.java
@@ -1,24 +1,30 @@
 package com.binance.app.Model;
 
 import lombok.Data;
+import org.hibernate.annotations.GenericGenerator;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Data
 @Entity
 public class Coin {
     @Id
-    private long id;
     private String symbol;
     private double price;
+
+    public Coin(){}
+    public Coin(String symbol, double price) {
+        this.symbol = symbol;
+        this.price = price;
+    }
 
     @Override
     public String toString() {
         return "Coin{" +
-                "id=" + id +
-                ", symbol='" + symbol + '\'' +
-                ", price=" + price +
+                "symbol = " + symbol + '\'' +
+                ", price = " + price +
                 '}';
     }
 }
