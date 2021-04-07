06b83a33cd3f4871298451d5a506c0670aeb0f55
mharikmert <mhilmiarikmert@gmail.com>
2021-04-07 16:54:30 +0300

    new rest endpoints that produces current time data, historical data and average price

--- web-server/src/main/java/com/binance/app/Controller/CoinRestController.java
+++ web-server/src/main/java/com/binance/app/Controller/CoinRestController.java
@@ -1,18 +1,19 @@
 package com.binance.app.Controller;
 
+import com.binance.app.Model.SubCoin;
 import com.binance.app.Repository.CoinRepository;
 import com.binance.app.Model.Coin;
+import com.binance.app.Repository.SubCoinRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.configurationprocessor.json.JSONArray;
 import org.springframework.boot.configurationprocessor.json.JSONException;
 import org.springframework.boot.configurationprocessor.json.JSONObject;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 @RestController
@@ -30,24 +31,25 @@ public class CoinRestController {
     }
 
     //injects restTemplate bean to fetch data
-    private RestTemplate restTemplate;
     @Autowired
-    public void setRestTemplate (RestTemplate restTemplate){
-        this.restTemplate = restTemplate;
-    }
-
-    //inject the coin repository to use CRUD operations
+    private RestTemplate restTemplate;
+    //inject entity repositories for CRUD operations
+    @Autowired
     private CoinRepository coinRepository;
 
     @Autowired
-    public void setCoinRepository(CoinRepository coinRepository) {
-        this.coinRepository = coinRepository;
-    }
+    private SubCoinRepository subCoinRepository;
 
-    @RequestMapping(value = "/api/v1/coins", method = RequestMethod.POST)
-    public ResponseEntity<?> saveCoins(@RequestBody Coin coin) {
-        coinRepository.save(coin);
+
+    @RequestMapping(value = "/api/v1/subCoins", method = RequestMethod.POST)
+    public ResponseEntity<?> saveCoins(@RequestBody SubCoin subCoin) {
+        System.out.println(subCoin);
+        subCoinRepository.save(subCoin);
         return ResponseEntity.ok().build();
+    }
+    @RequestMapping(value = "/api/v1/subCoins", method = RequestMethod.GET)
+    public ResponseEntity<?> showSubCoins() {
+        return ResponseEntity.ok(subCoinRepository.findAll());
     }
 
     @RequestMapping(value = "api/v1/coins", method = RequestMethod.GET)
@@ -74,5 +76,66 @@ public class CoinRestController {
             coinRepository.save(new Coin(symbol, price));
         }
         return ResponseEntity.ok(data);
+    }
+
+    //serves requested coin info dynamically
+    @RequestMapping(
+            value = "/api/v1/klines",
+            method = RequestMethod.GET)
+    public ResponseEntity<?> avgPrice(
+            //required true by default
+            @RequestParam("symbol") String symbol,
+            @RequestParam("interval") String interval,
+            @RequestParam(value = "startTime",required = false) Long startTime,
+            @RequestParam(value = "endTime", required = false) Long endTime,
+            @RequestParam(value = "limit",required = false) Long limit)
+    {
+
+        //dynamically created url with parameters
+        String url = "https://api.binance.com/api/v3/klines?symbol=" + symbol + "&interval=" + interval;
+        if(startTime != null) url += "&startTime=" + startTime;
+        if(endTime != null) url += "&endTime=" + endTime;
+        if(limit != null) url += "&limit=" + limit;
+        try{
+            //fetch data from url and serveV
+            Object response = restTemplate.getForObject(url, Object.class);
+            //return ResponseEntity.ok(restTemplate.getForObject(url, Object.class));
+            return ResponseEntity.ok(response);
+        }
+        catch(Exception ex){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+    //serves average price in last 5 min
+    @RequestMapping(value = "api/v1/avgPrice", method = RequestMethod.GET)
+    public ResponseEntity<?> averagePrice(@RequestParam("symbol") String symbol){
+        System.out.println(symbol);
+        String url = "https://api.binance.com/api/v3/avgPrice?symbol=" + symbol;
+        Object response = restTemplate.getForObject(url, Object.class);
+        return ResponseEntity.ok(response);
+    }
+
+    @RequestMapping(
+            value = "api/v1/hourAvg",
+            method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> hourAverage(@RequestParam("symbol") String symbol) throws JSONException {
+        String url = "https://api.binance.com/api/v3/klines?symbol=" + symbol + "&interval=1h&limit=1";
+        //fetches hourly data
+        Object [] response = restTemplate.getForObject(url, Object[].class);
+        assert response != null;
+        JSONArray obj = new JSONArray(response);
+        JSONArray jsonArray = (JSONArray) obj.get(0);
+        double averagePrice = ( Double.parseDouble((String) jsonArray.get(2)) +
+                                Double.parseDouble((String) jsonArray.get(3)) ) / 2;
+
+
+        //produces json object
+        JSONObject json = new JSONObject();
+        json.put("symbol", symbol);
+        json.put("averagePrice",averagePrice);
+
+        return ResponseEntity.ok(String.valueOf(json));
     }
 }
